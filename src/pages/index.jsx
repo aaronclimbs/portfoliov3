2d5a77801d4ef001cb29872b3a1cd6c04ba53278
Aaron Keisler <amkeisler@gmail.com>
2021-07-16 14:33:51 -0400

    feat: add in signin page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import HeroSection from '../components/HeroSection'
-import InfoSection from '../components/InfoSection'
-import { homeObjOne } from '../components/InfoSection/Data'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import { useState } from "react";
+import HeroSection from "../components/HeroSection";
+import InfoSection from "../components/InfoSection";
+import { homeObjOne } from "../components/InfoSection/Data";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import ServicesSection from "../components/ServicesSection";
 
 const Home = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen)
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
   return (
     <>
-      <Sidebar toggleSidebar={toggleSidebar} isOpen={isSidebarOpen} data-testid="sidebar" />
+      <Sidebar
+        toggleSidebar={toggleSidebar}
+        isOpen={isSidebarOpen}
+        data-testid="sidebar"
+      />
       <Navbar toggleSidebar={toggleSidebar} data-testid="navbar" />
-      <HeroSection/>
-      {homeObjOne.map((item, index) => (<InfoSection {...item} key={index}/>))}
+      <HeroSection />
+      {homeObjOne.map((item, index) => (
+        <InfoSection {...item} key={index} />
+      ))}
+      <ServicesSection />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
